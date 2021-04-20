f6f244a0330effc500e65705bc01761c29bb6b3b
hcampbell1212 <heather.campbell1212@gmail.com>
2021-04-20 11:21:30 -0500

    Scattergories created all components and added styling to App, Letter and Timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Timer from './timer/Timer';
+import Letter from './letter/Letter';
+import Categories from './categories/Categories';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-Section-One'>
+        <Letter />
+        <Timer />
+        <Categories />
+      </div>
+      <div className='App-Section-Two'>
+
+      </div>
     </div>
   );
 }
